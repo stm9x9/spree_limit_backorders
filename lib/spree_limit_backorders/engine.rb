344c7eb9239a4d3935f4df3a5c91763550304ce0
Josh Nussbaum <joshnuss@gmail.com>
2015-05-29 01:36:51 -0400

    Add max_backorders to permitted params

--- lib/spree_limit_backorders/engine.rb
+++ lib/spree_limit_backorders/engine.rb
@@ -9,6 +9,10 @@ module SpreeLimitBackorders
       g.test_framework :rspec
     end
 
+    initializer 'spree_limit_backorders.permitted_attributes' do
+      Spree::PermittedAttributes.stock_item_attributes << :max_backorders
+    end
+
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
